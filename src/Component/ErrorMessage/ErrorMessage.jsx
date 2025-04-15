3917fa6015ddd55cd2dbc8b07aeb4fedb97417c0
PuDDDle <volodabelihin35@gmail.com>
2025-04-15 12:03:30 +0300

    upd

--- src/Component/ErrorMessage/ErrorMessage.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ErrorMessage = ({ message = "Щось пішло не так. Спробуй ще раз." }) => {
-  return <p style={{ color: "red", textAlign: "center" }}>{message}</p>;
-};
-
-export default ErrorMessage;
